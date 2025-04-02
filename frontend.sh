e177d15ae8bb8af3ec49888c004ba505eeb486a8
samsung150973 <samsung150973@gmail.com>
2025-04-02 12:53:31 +0100

    update

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
 echo "I am frontend"
+
+# this command will stop script if any error
+set -e
+
+# install nginix
+yum install nginx -y >> nginixinstall.log
+
+#download the frontend file from the loation
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+#Clear the folder & Deploy the new frontend file in the html folder
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+#restart nginx
+systemctl enable nginx
+systemctl start nginx
