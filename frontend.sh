2822819c40c6941df5e6743c4754f12a0987c0c3
samsung150973 <samsung150973@gmail.com>
2025-04-02 18:56:31 +0100

    update

--- frontend.sh
+++ frontend.sh
@@ -4,6 +4,7 @@ echo "I am frontend"
 # this command will stop script if any error
 set -e
 
+# check if the executed as a root user. in lunix root user id is 0 "id -u". Assign output to a variable and then check
 ID=$(id -u)
 if [ "$ID" -ne 0 ]; 
 then
@@ -12,7 +13,15 @@ then
 fi
 
 # install nginix
+echo " installing nginx"
 yum install nginx -y >> nginixinstall.log
+
+# check if nginix installition is successful and print a message
+if [ $? -ne 0 ];
+then
+    echo -e "\e[33m Nginix installation successful \e[0m"
+fi
+
 
 #download the frontend file from the loation
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
