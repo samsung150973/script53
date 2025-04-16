2f07f0853dbcb07ce326f94c77b0ff2be217aee5
samsung150973 <samsung150973@gmail.com>
2025-04-16 21:31:31 +0100

    updates

--- frontend.sh
+++ frontend.sh
@@ -2,19 +2,20 @@
 echo "I am frontend"
 logfile = $install.log
 
-Check ()
-if [ $? -ne 0 ];
+Check (){
+if [ $1 -ne 0 ];
 then
-    echo -e "\e[33m installation unsuccessful \e[0m"
+    echo -e "\e[33m unsuccessful \e[0m"
     exit 2
 else 
-    echo -e "\e[33m installation successful \e[0m"
+    echo -e "\e[33m successful \e[0m"
 fi
+}
 
 # this command will stop script if any error
 set -e
 
-# check if the executed as a root user. in lunix root user id is 0 "id -u". Assign output to a variable and then check
+# check if root user. in lunix root user id is 0 "id -u". Assign output to a variable and then check
 ID=$(id -u)
 if [ "$ID" -ne 0 ]; 
 then
@@ -24,32 +25,19 @@ fi
 
 # install nginix
 echo -n "installing nginx :"
-yum install nginx -y &>> "$logfile"
+yum install nginx-y &>> "$logfile"
+
 
 # check if nginix installition is successful and print a message
-check ()
-# if [ $? -ne 0 ];
-# then
-#     echo -e "\e[33m Nginix installation unsuccessful \e[0m"
-#     exit 2
-# else 
-#     echo -e "\e[33m Nginix installation successful \e[0m"
-# fi
+check $?
 
 
 #download the frontend file from the loation
-echo -n "instaling frontend :"
+echo -n "installing frontend :"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 # check if frontend download successful and print a message
-check()
-# if [ $? -ne 0 ];
-# then
-#     echo -e "\e[33m fronend download unsuccessful \e[0m"
-#     exit 3
-# else 
-#     echo -e "\e[33m frontend installation successful \e[0m"
-# fi
+check $?
 
 #Clear the folder & Deploy the new frontend file in the html folder
 cd /usr/share/nginx/html
@@ -61,6 +49,7 @@ rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
 #restart nginx
+echo -n "restarting Nginix Server :"
 systemctl enable nginx &>> "$logfile"
 systemctl start nginx &>> "$logfile"
-echo -e "\e[33m Nginix restart successful \e[0m"
+check $?
